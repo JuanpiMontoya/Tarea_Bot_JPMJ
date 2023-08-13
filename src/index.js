03de6922a8f8b9735608467fdbb5208846d1b31a
Juan Pablo Montoya <juan.montoya@ucb.edu.bo>
2023-08-13 01:05:43 -0400

    Tarea Bot Saludo según Idioma

--- src/index.js
+++ src/index.js
@@ -3,42 +3,86 @@ const genero = document.querySelector("#input_Genero");
 const edad = document.querySelector("#input_Edad");
 const form = document.querySelector("#form_saludo");
 const saludo = document.querySelector("#mensajes_Bot");
+var idioma = navigator.language || navigator.userLanguage;
+let fechaActual = new Date();
+let horaActual = fechaActual.getHours();
+let saludo_hora;
+            
+function verificarHora() {
+    //Verificamos la hora
+    if (idioma.startsWith("es")) 
+    {
+        if (horaActual >= 6 && horaActual < 12) 
+        {
+            saludo_hora = "Buenos dias";
+        } 
+        else if (horaActual >= 12 && horaActual < 18)
+        {
+            saludo_hora = "Buenas tardes";
+        } 
+        else 
+        {
+            saludo_hora = "Buenas noches";
+        }
+    } 
+    else if (idioma.startsWith("en")) 
+    {
+        if (horaActual >= 6 && horaActual < 12) 
+        {
+            saludo_hora = "Good morning";
+        } 
+        else if (horaActual >= 12 && horaActual < 18)
+        {
+            saludo_hora = "Good afternoon";
+        } 
+        else 
+        {
+            saludo_hora = "Good Night";
+        }
+    }   
+}
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     var saludo_bot
-    let fechaActual = new Date();
-    let horaActual = fechaActual.getHours();
-    let saludo_hora;
-    //Verificamos la hora
-    if (horaActual >= 6 && horaActual < 12) 
-    {
-        saludo_hora = "Buenos dias";
-    } 
-    else if (horaActual >= 12 && horaActual < 18)
-    {
-        saludo_hora = "Buenas tardes";
-    } 
-    else 
-    {
-        saludo_hora = "Buenas noches";
-    }
+    verificarHora()
     //Formamos el saludo
-    if(genero.value === "mujer")
+    if (idioma.startsWith("es")) 
     {
-        saludo_bot = "Hola Sra " + nombre.value + ", " + saludo_hora;
-    }
-    else
-    {
-        if(genero.value === "varon" && edad.value > 30)
+        if(genero.value === "mujer")
         {
-            saludo_bot = "Hola Sr " + nombre.value + ", " + saludo_hora;
+        saludo_bot = "Hola Sra " + nombre.value + ", " + saludo_hora;
         }
         else
         {
-            saludo_bot = "Hola " + nombre.value + ", " + saludo_hora;
-        }  
-    }
+            if(genero.value === "varon" && edad.value > 30)
+            {
+                saludo_bot = "Hola Sr " + nombre.value + ", " + saludo_hora;
+            }
+            else
+            {
+                saludo_bot = "Hola " + nombre.value + ", " + saludo_hora;
+            }  
+        }
+    } 
+    else if (idioma.startsWith("en")) 
+    {
+        if(genero.value === "mujer")
+        {
+        saludo_bot = "Hello Ms. " + nombre.value + ", " + saludo_hora;
+        }
+        else
+        {
+            if(genero.value === "varon" && edad.value > 30)
+            {
+                saludo_bot = "Hello Mr. " + nombre.value + ", " + saludo_hora;
+            }
+            else
+            {
+                saludo_bot = "Hello " + nombre.value + ", " + saludo_hora;
+            }  
+        }
+    } 
     saludo.style.visibility = "visible";
     saludo.textContent = saludo_bot;
 });
