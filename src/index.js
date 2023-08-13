0f3042e3f43a10cb69e0592661a6384b53a27a6e
Juan Pablo Montoya <juan.montoya@ucb.edu.bo>
2023-08-13 00:21:45 -0400

    Tarea Bot Saludo Nombre

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './App';
+const nombre = document.querySelector("#input_Nombre");
+const form = document.querySelector("#form_saludo");
+const saludo = document.querySelector("#mensajes_Bot");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    saludo.style.visibility = "visible";
+    saludo.textContent = "Hola " + nombre.value;
+});
