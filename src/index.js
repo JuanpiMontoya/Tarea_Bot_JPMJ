4f127df2073f1e8847017939c062c96bff256195
Juan Pablo Montoya <juan.montoya@ucb.edu.bo>
2023-08-13 00:42:13 -0400

    Tarea Bot Saludo Edad

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const nombre = document.querySelector("#input_Nombre");
 const genero = document.querySelector("#input_Genero");
+const edad = document.querySelector("#input_Edad");
 const form = document.querySelector("#form_saludo");
 const saludo = document.querySelector("#mensajes_Bot");
 
@@ -12,7 +13,14 @@ form.addEventListener("submit", (event) => {
     }
     else
     {
-        saludo_bot = "Hola " + nombre.value;
+        if(genero.value === "varon" && edad.value > 30)
+        {
+            saludo_bot = "Hola Sr " + nombre.value;
+        }
+        else
+        {
+            saludo_bot = "Hola " + nombre.value;
+        }  
     }
     saludo.style.visibility = "visible";
     saludo.textContent = saludo_bot;
