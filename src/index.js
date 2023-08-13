196c0ef0b69051fca24b3133fcdd776607c73117
Juan Pablo Montoya <juan.montoya@ucb.edu.bo>
2023-08-13 00:34:09 -0400

    Tarea Bot Saludo Genero

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const nombre = document.querySelector("#input_Nombre");
+const genero = document.querySelector("#input_Genero");
 const form = document.querySelector("#form_saludo");
 const saludo = document.querySelector("#mensajes_Bot");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    var saludo_bot
+    if(genero.value === "mujer")
+    {
+        saludo_bot = "Hola Sra " + nombre.value;
+    }
+    else
+    {
+        saludo_bot = "Hola " + nombre.value;
+    }
     saludo.style.visibility = "visible";
-    saludo.textContent = "Hola " + nombre.value;
+    saludo.textContent = saludo_bot;
 });
